3b9ca01ab1cf0d847510e90ccda004d220650fd3
AlexS997 <64364829+AlexS997@users.noreply.github.com>
2026-04-15 17:50:18 +0200

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://alex_db_user:UE0sf3sSNAZC8Am6@cluster0.1kfmsfo.mongodb.net/WatchSite").then(() => console.log('DB CONNECTEDD'))
+    await mongoose.connect("").then(() => console.log('DB CONNECTEDD'))
 }
